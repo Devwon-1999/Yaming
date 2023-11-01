b96ec2699b7291b27700fc591887e55e9f465575
Devwon-1999 <Devwon.1999@gmail.com>
2023-11-01 20:35:12 +0900

    대화상자 구현

--- app/src/main/java/com/example/yaming/CustomDialog.kt
+++ app/src/main/java/com/example/yaming/CustomDialog.kt
@@ -5,6 +5,9 @@ import android.content.Context
 import android.os.Bundle
 import android.view.Window
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
 
 
 class CustomDialog(context: Context) : Dialog(context) {
@@ -14,10 +17,43 @@ class CustomDialog(context: Context) : Dialog(context) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.userinput_custom_dialog)
 
-        // 여기에서 custom_dialog_layout.xml의 뷰 요소에 대한 작업을 수행하세요.
+        val btSaveDialog = findViewById<Button>(R.id.btSaveDialog)
+        btSaveDialog.setOnClickListener {
+            val EditTextoneMealTotalCal = findViewById<EditText>(R.id.oneMealTotalCal)
+            val oneMealTotalCal = EditTextoneMealTotalCal.text.toString()
+
+            val EditTextoneMealTotalTan = findViewById<EditText>(R.id.oneMealTotalTan)
+            val oneMealTotalTan = EditTextoneMealTotalTan.text.toString()
+
+            val EditTextoneMealTotalDan = findViewById<EditText>(R.id.oneMealTotalDan)
+            val oneMealTotalDan = EditTextoneMealTotalDan.text.toString()
+
+            val EditTextoneMealTotalJi = findViewById<EditText>(R.id.oneMealTotalJi)
+            val oneMealTotalJi = EditTextoneMealTotalJi.text.toString()
+
+            val radioBreakfast = findViewById<RadioButton>(R.id.radioBreakfast)
+            val radioLunch = findViewById<RadioButton>(R.id.radioLunch)
+            val radioDinner = findViewById<RadioButton>(R.id.radioDinner)
+            var meal: String = ""
+            if(radioBreakfast.isChecked()){
+                meal = "아침"
+            }
+            else if(radioLunch.isChecked()){
+                meal = "점심"
+            }
+            else if(radioLunch.isChecked()){
+                meal = "저녁"
+            }
+            else{
+                Toast.makeText(context, "식사를 선택하세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+        }
+
         val btCloseDialog = findViewById<Button>(R.id.btCloseDialog)
         btCloseDialog.setOnClickListener {
-            // 다이얼로그를 닫기 위한 동작을 추가하세요.
+
             dismiss()
         }
     }
